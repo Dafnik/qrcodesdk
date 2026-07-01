5901bc103bda13a775e7622583272f766d09a7d4
Dafnik <git@dafnik.me>
2026-07-01 13:32:16 +0200

    feat: qrcode matrix generator v1

---
+++ packages/core/src/svg.ts
@@ -0,0 +1,35 @@
+import {generateQrCodeMatrix} from './matrix';
+import type {generateOptions, generateWithAccessibleOptions} from './types';
+
+export function generateQrCodeSVGString(
+  data: string | number,
+  options: generateOptions & generateWithAccessibleOptions = {},
+): string {
+  const matrix = generateQrCodeMatrix(data, options);
+  const modSize = options.size ?? 5;
+  const margin = options.margin ?? 4;
+  const fgColor = options.colors?.colorLight ?? '#ffffff';
+  const bgColor = options.colors?.colorDark ?? '#000000';
+  const n = matrix.length;
+  const size = modSize * (n + 2 * margin);
+
+  const xml = [
+    '<svg xmlns="http://www.w3.org/2000/svg"',
+    `width="${size}" height="${size}" ${options.alt ? `alt="${options.alt}"` : ''} ${options.ariaLabel ? `aria-label="${options.ariaLabel}"` : ''} ${options.title ? `title="${options.title}"` : ''}>`,
+  ];
+  xml.push(`<rect width="${size}" height="${size}" fill="${fgColor}" />`);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j]) {
+        xml.push(
+          `<rect x="${modSize * (margin + j)}" y="${modSize * (margin + i)}" width="${modSize + 0.3}" height="${modSize + 0.3}" fill="${bgColor}" />`,
+        );
+      }
+    }
+  }
+
+  xml.push('</svg>');
+
+  return xml.join('\n');
+}
---
+++ packages/core/src/types.ts
@@ -0,0 +1,70 @@
+export type QRCodeVersion =
+  | -1
+  | 0
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15
+  | 16
+  | 17
+  | 18
+  | 19
+  | 20
+  | 21
+  | 22
+  | 23
+  | 24
+  | 25
+  | 26
+  | 27
+  | 28
+  | 29
+  | 30
+  | 31
+  | 32
+  | 33
+  | 34
+  | 35
+  | 36
+  | 37
+  | 38
+  | 39
+  | 40;
+
+export type QRCodeErrorCorrectionLevel = 'L' | 'M' | 'Q' | 'H';
+export type QRCodeMode = 'numeric' | 'alphanumeric' | 'octet';
+
+export type ColorValueHex = `#${string}`;
+
+export type generateMatrixOptions = {
+  version?: QRCodeVersion;
+  mode?: QRCodeMode;
+  errorCorrectionLevel?: QRCodeErrorCorrectionLevel;
+  mask?: number;
+};
+
+export type generateOptions = generateMatrixOptions & {
+  size?: number;
+  margin?: number;
+  colors?: {
+    colorLight?: ColorValueHex;
+    colorDark?: ColorValueHex;
+  };
+};
+
+export type generateWithAccessibleOptions = {
+  alt?: string;
+  ariaLabel?: string;
+  title?: string;
+};
