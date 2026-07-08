607890dcb39a1a4f8dc0b26a31172217c071fea7
Dafnik <git@dafnik.me>
2026-07-08 20:19:58 +0200

    build: add angular-eslint

--- packages/angular/eslint.config.mjs
+++ packages/angular/eslint.config.mjs
@@ -1,8 +1,30 @@
 import {config} from '@repo/eslint-config/base';
+import angular from 'angular-eslint';
+
+const withFiles = (configs, files) => configs.map((entry) => ({...entry, files}));
 
 export default [
   ...config,
   {
     ignores: ['dist/**', '.angular/**', 'out-tsc/**'],
   },
+  ...withFiles(angular.configs.tsRecommended, ['projects/**/*.ts']),
+  {
+    files: ['projects/**/*.ts'],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: '',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  ...withFiles(
+    [...angular.configs.templateRecommended, angular.configs.templateAccessibility.at(-1)],
+    ['projects/**/*.html'],
+  ),
 ];
