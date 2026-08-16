a89722d62aeee6227c4f294106b667508350bfaf
Dafnik <git@dafnik.me>
2026-08-16 13:05:23 +0200

    chore: feedback

--- apps/docs/src/components/svelte/qrcode-svg-example.svelte
+++ apps/docs/src/components/svelte/qrcode-svg-example.svelte
@@ -2,7 +2,11 @@
   import type {QRCodeSVGOptions} from '@qrcodesdk/core';
   import {QRCodeSVG} from '@qrcodesdk/svelte';
 
-  const options: QRCodeSVGOptions = {size: 8, margin: 2};
+  const options: QRCodeSVGOptions = {
+    size: 8,
+    margin: 2,
+    ariaLabel: 'Scan to open qrcodesdk.dev',
+  };
 </script>
 
 <QRCodeSVG class="mx-auto" data="https://qrcodesdk.dev" {options} />
