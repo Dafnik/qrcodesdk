d4cadde263ca03ae1d6099306020f20523948c67
Dafnik <git@dafnik.me>
2026-08-23 14:03:28 +0200

    docs: improve getting started and examples

--- apps/docs/src/components/angular/qrcode-download-image-example.ts
+++ apps/docs/src/components/angular/qrcode-download-image-example.ts
@@ -6,7 +6,7 @@ import {QRCodeImage} from '@qrcodesdk/angular';
   selector: 'qrcode-angular-download-image-example',
   imports: [QRCodeImage],
   template: `
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center gap-2">
       <qrcode-image
         #qrcode
         [options]="{
--- apps/docs/src/components/svelte/qrcode-download-image-example.svelte
+++ apps/docs/src/components/svelte/qrcode-download-image-example.svelte
@@ -6,7 +6,7 @@
   const options: QRCodeImageOptions = {alt: 'QR code for qrcodesdk.dev'};
 </script>
 
-<div class="flex flex-col items-center">
+<div class="flex flex-col items-center gap-2">
   <QRCodeImage bind:this={qrcode} data="https://qrcodesdk.dev" {options} />
   <button class="btn-primary" type="button" onclick={() => qrcode?.download('qrcodesdk')}>
     Download PNG
